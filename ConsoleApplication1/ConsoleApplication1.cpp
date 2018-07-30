62d3e2ff5fcf7470947d3cb254d64f291c470ff2
cqwei <cqwei@DESKTOP-13DKM7R>
2018-07-30 13:59:43 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	long lVer = _MSC_FULL_VER;
+	printf("_MSC_FULL_VER = %d\n", _MSC_FULL_VER);
+    return 0;
+}
+
